5f4e7431343baf1b5b97c5bef4b749183fa26deb
CaueMiyazaki <83715093+CaueAlvim@users.noreply.github.com>
2025-03-11 01:15:52 -0300

    Update on setupHyprland.sh

--- setupHyprland.sh
+++ setupHyprland.sh
@@ -9,19 +9,25 @@ fi
 sudo pacman -Syu --noconfirm
 sleep 0.5
 
+# Install core packages
+sudo pacman -S hyprland hypridle hyprlock xdg-desktop-portal-hyprland xdg-user-dirs ufw fwupd sbctl nano --noconfirm
+sleep 0.5
+
 # Install essential packages
-sudo pacman -S hyprland hypridle hyprlock kitty waybar swaync xdg-desktop-portal-hyprland xdg-user-dirs ufw fwupd sbctl nano grim slurp swww ttf-dejavu ttf-dejavu-nerd cantarell-fonts --noconfirm
+sudo pacman -S hyprpolkitagent hyprsunset kitty waybar swaync grim slurp greetd fuzzel swww ttf-dejavu ttf-dejavu-nerd cantarell-fonts --noconfirm
 sleep 0.5
 
 # Install some other stuff
-sudo pacman -S imagemagick hyprpolkitagent hyprsunset opus opusfile mesa vulkan-radeon greetd fuzzel starship --noconfirm
+sudo pacman -S gvfs-mtp imagemagick ffmpeg opus opusfile mesa vulkan-radeon --noconfirm
 sleep 0.5
 
 # Install apps
-sudo pacman -S swayimg gvfs-mtp btop thunar thunar-volman tumbler --noconfirm
+sudo pacman -S swayimg btop thunar thunar-volman tumbler starship --noconfirm
 sleep 0.5
 
+mkdir -p $HOME/.config
+cp -r dotfiles/ $HOME/.config/
+
 clear
-echo "receba"
 echo "ATIVAR ufw E greetd, substitua o comando default no arquivo /etc/greetd/config.toml"
 echo "Atualizar firmware e rodar xdg-user-dirs-update"
